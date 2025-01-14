3e8804b0079ec373f5388ddc0516b2d3e8a7e369
benshabbat <benshabbat27@gmail.com>
2025-01-14 14:51:13 +0200

    some conditions and for

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,10 +3,18 @@ export default {
   data() {
     return {
       name: 'Vue.js',
+      status: "active",
+      tasks:["task1","task2"],
     }
   }
 }
 </script>
 <template>
   <h1>{{ name }}</h1>
+  <h2 v-if="status==='active'">{{ "is active" }}</h2>
+  <h2 v-else-if="status==='pending'">{{ "is pending" }}</h2>
+  <h2 v-else>{{ "is inactive" }}</h2>
+  <ul>
+    <li v-for="task in tasks" :key="task" >{{ task }}</li>
+  </ul>
 </template>
