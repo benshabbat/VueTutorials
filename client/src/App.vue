2a1163f73d14eeb17c54196713169714ade22fb5
benshabbat <benshabbat27@gmail.com>
2025-01-14 19:45:09 +0200

    addTask form input label submit

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,6 +4,7 @@ const name = ref('Vue.js');
 const status = ref('active');
 const tasks = ref(["task1", "task2"]);
 const link = ref("http://www.google.com");
+const newTask = ref("");
 
 const changeStatus = () => {
   if (status.value == "active") {
@@ -14,12 +15,25 @@ const changeStatus = () => {
     status.value = "active";
   }
 }
+
+function addTask() {
+  if (newTask.value.trim() === "") return;
+  tasks.value.push(newTask.value);
+  newTask.value = "";
+
+}
 </script>
 <template>
   <h1>{{ name }}</h1>
   <h2 v-if="status === 'active'">{{ "is active" }}</h2>
   <h2 v-else-if="status === 'pending'">{{ "is pending" }}</h2>
   <h2 v-else>{{ "is inactive" }}</h2>
+  <form @submit.prevent="addTask">
+    <label for="newTask">New Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask">
+    <button type="submit">Add Task</button>
+
+  </form>
   <ul>
     <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
