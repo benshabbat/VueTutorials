39e7ad3faaaee71a33961a5847bf41c0ddda222b
benshabbat <benshabbat27@gmail.com>
2025-01-15 11:09:50 +0200

    logo comonent

---
+++ client/src/components/Logo.vue
@@ -0,0 +1,10 @@
+<script setup>
+import logo from '../assets/img/logo.png';
+</script>
+<template>
+
+    <a class="flex flex-shrink-0 items-center mr-4" href="index.html">
+        <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
+        <span class="hidden md:block text-white text-2xl font-bold ml-2">Vue Jobs</span>
+    </a>
+</template>
