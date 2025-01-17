8126e064f4ef829ddf6ee3a299517d5509e06469
benshabbat <benshabbat27@gmail.com>
2025-01-17 11:44:43 +0200

    made filerjobs comp and using with joblist and filter job for jobview

---
+++ client/src/components/FilterJobs.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+    <section class="bg-green-50 py-4">
+        <div class="container mx-auto px-4">
+            <div class="relative">
+                <input type="text"
+                    class="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-green-500"
+                    placeholder="Filter jobs..." />
+            </div>
+        </div>
+    </section>
+</template>
--- client/src/views/JobsView.vue
+++ client/src/views/JobsView.vue
@@ -1,5 +1,10 @@
-<script setup></script>
+<script setup>
+import FilterJobs from '@/components/FilterJobs.vue';
+import JobListing from '@/components/JobListing.vue';
+
+</script>
 
 <template>
-    
+    <FilterJobs />
+    <JobListing />
 </template>
