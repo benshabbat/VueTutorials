0993116d0e21dba5fbc1f2efc95c06c7c15f60bb
benshabbat <benshabbat27@gmail.com>
2025-01-17 10:58:07 +0200

    home view using router-vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,7 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
-import Hero from './components/Hero.vue';
-import HomeCards from './components/HomeCards.vue';
-import JobListing from './components/JobListing.vue';
 </script>
 <template>
   <Navbar/>
-  <Hero/>
-  <HomeCards/>
-  <JobListing :limit="3" :showButton="true"/>
+  <router-view/>
 </template>
